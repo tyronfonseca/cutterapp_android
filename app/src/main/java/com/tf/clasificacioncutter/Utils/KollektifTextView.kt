de89c7ba0fd9d5fb86b76d2b72c94b7ee8ed2d74
Tyron Fonseca <tyron.fonseca@hotmail.com>
2022-02-26 12:04:44 -0600

    Update app building config...
    -Now it uses viewBinding
    -Target is now Android 12
    -Remove unnecessary comments
    -Fixed errors with CutterHelper.kt
    -Update About info
    -Update dependencies versions

--- app/src/main/java/com/tf/clasificacioncutter/Utils/KollektifTextView.kt
+++ app/src/main/java/com/tf/clasificacioncutter/Utils/KollektifTextView.kt
@@ -7,12 +7,9 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.res.ResourcesCompat
 import com.tf.clasificacioncutter.R
 
-class KollektifTextView(context: Context?, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
+class KollektifTextView(context: Context?, attrs: AttributeSet?) : AppCompatTextView(context!!, attrs) {
     init {
         val typeface: Typeface? = ResourcesCompat.getFont(getContext(), R.font.kollektif)
-        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB ||
-               android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.HONEYCOMB_MR2){
-            setTypeface(typeface)
-        }
+        setTypeface(typeface)
     }
 }
